5144abca93bd059333be3652f262d8e4db47f2e2
Tomasz Cudziło <tomasz@luzid.co>
2012-03-01 00:00:21 +0100

    Add script doing ROT with custom step

---
+++ rotXX.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import fileinput, sys, string
+
+rot_by = int(sys.argv[1])
+# print "rotating by %d" % rot_by
+
+from_chars = string.ascii_uppercase + string.ascii_lowercase
+to_chars = ""
+alphabet_length = len(from_chars)/2
+for half in (0,1):
+    offset = half * alphabet_length
+    for i in range(offset, offset + alphabet_length):
+        to_chars += from_chars[offset + (i+rot_by) % alphabet_length]
+# print from_chars
+# print to_chars
+
+translate_table = string.maketrans(from_chars, to_chars)
+
+for line in fileinput.input(sys.argv[2:]):
+    line = line.strip()
+    print line.translate(translate_table)
